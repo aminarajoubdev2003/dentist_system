6fba964e5285a695b209a9aa7c15d9aab83e048f
aminarajoubdev2003 <150340884+aminarajoubdev2003@users.noreply.github.com>
2024-08-04 18:41:27 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,3 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/add_pat', [App\Http\Controllers\PatientController::class, 'create'])->name('create_pat');
-Route::post('/add_pat', [App\Http\Controllers\PatientController::class, 'store'])->name('add_pat');
-Route::get('/add_appointment', [App\Http\Controllers\AppointmentController::class, 'create'])->name('create_appoin');
-Route::post('/add_appointment', [App\Http\Controllers\AppointmentController::class, 'store'])->name('add_appoin');
-Route::get('/edit_appointment/{id}', [App\Http\Controllers\AppointmentController::class, 'edit'])->name('edit_appoin');
-Route::post('/edit_appointment/{id}', [App\Http\Controllers\AppointmentController::class, 'update'])->name('update_appoin');
-Route::delete('/delete_appointment/{id}', [App\Http\Controllers\AppointmentController::class, 'destroy'])->name('delete_appoin');
-Route::get('/all_appointment/{doctor_id}', [App\Http\Controllers\AppointmentController::class, 'index'])->name('all_appoin');
-Route::get('/all_pat', [App\Http\Controllers\PatientController::class, 'index'])->name('all_pat');
